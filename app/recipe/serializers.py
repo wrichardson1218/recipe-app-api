04c7d334775235bbbcffbda75df8d5e12abf471c
Will Richardson <wrichardson1218@gmail.com>
2020-10-09 02:01:29 -0400

    Added create ingredient feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -16,6 +16,7 @@ class IngredientSerializer(serializers.ModelSerializer):
     """Serializer for an ingredient object"""
 
     class Meta:
+
         model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
